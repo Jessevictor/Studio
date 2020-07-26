f44b7682761534e9f0e5014d1257888732c34eea
jessevictor <kongoijesse97@gmail.com>
2020-07-26 22:22:33 +0300

    add asset file

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $(".num1").click(function(){
+        $(".num1").hide();
+
+ });
+  $(".#hello").click(function(){
+      $(".num1").toggle();
+  });
+});
+// $(document).ready(function(){
+//     $("#hello").click(function(){
+//         $(".num1").hide();
+
+//  });
+//   $(".num1").click(function(){
+//       $("#hello").show();
+//   });
+// });
--- script.js
+++
@@ -1,5 +0,0 @@
-watermark(['image/logo.png'])
-    .image(watermark.image.lowerRight(0.5)) // Or lowerRight() for no opacity
-    .then(function (img) {
-        document.getElementById('alpha-image').appendChild(img);
-    });
